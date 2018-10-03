277db2f97749cef4cdfcfa1f7d584658773f5490
Pascal Keller <pascal_keller@web.de>
2018-10-03 20:56:38 +0200

    Update Main

--- Main.py
+++ Main.py
@@ -9,15 +9,21 @@ NON_VEHICLE_IMAGES_DIR = 'TrainingData/NonVehicles'
 
 def create_model():
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3)))
-    model.add(MaxPooling2D((2, 2)))
+    model.add(Cropping2D(input_shape=(64, 64, 3)))
+    #model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(65, 320, 3)))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Flatten())
-    #model.add(Dense(128))
-    #model.add(Activation('relu'))
-    #model.add(Dense(5))
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
     model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
     return model
 
 
@@ -32,22 +38,21 @@ labels = []
 for img_file in vehicle_image_files:
     img = (misc.imread(img_file) - 128.0) / 128.0
     images.append(img)
-    labels.append(1)
+    labels.append(24.5)
 for img in non_vehicle_image_files:
     img = (misc.imread(img_file) - 128.0) / 128.0
     images.append(img)
-    labels.append(0)
+    labels.append(23.7)
 
 images = np.array(images)
 labels = np.array(labels)
 
-from sklearn.preprocessing import LabelBinarizer
+#from sklearn.preprocessing import LabelBinarizer
 from keras.utils import to_categorical
-label_binarizer = LabelBinarizer()
-y_one_hot = label_binarizer.fit_transform(labels)
+#label_binarizer = LabelBinarizer()
+#y_one_hot = label_binarizer.fit_transform(labels)
 
-model.compile('adam', 'binary_crossentropy', ['accuracy'])
-history = model.fit(images, y_one_hot, epochs=1, validation_split=0.2)
+history = model.fit(images, labels, epochs=1, validation_split=0.2, shuffle=True)
 exit(0)
 
 with open('small_test_traffic.p', 'rb') as f:
