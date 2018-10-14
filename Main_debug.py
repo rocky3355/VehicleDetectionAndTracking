681c25172035a7755e705d58efe50e57db25056f
Pascal Keller <pascal_keller@web.de>
2018-10-14 22:52:31 +0200

    Create seperate Main.py for debugging

--- Main_debug.py
+++ Main_debug.py
@@ -13,13 +13,6 @@ MODEL_IMG_SIZE = (64, 64)
 MODEL_FILE_NAME = 'model.h5'
 VEHICLE_IMAGES_DIR = 'TrainingData/Vehicles'
 NON_VEHICLE_IMAGES_DIR = 'TrainingData/NonVehicles'
-HEAT_MAP_ITERATIONS = 7
-HEAT_MAP_THRESHOLD = 100
-MODEL_CAR_THRESHOLD = 0.75
-
-iteration = 0
-heat_map = None
-last_labels = None
 
 
 class Window:
@@ -73,6 +66,18 @@ def train_model(model):
     model.save(MODEL_FILE_NAME)
 
 
+def test_model():
+    img1 = cv2.resize(misc.imread('TestImages/Car01.jpg'), MODEL_IMG_SIZE)
+    img2 = cv2.resize(misc.imread('TestImages/Car02.jpg'), MODEL_IMG_SIZE)
+    img3 = cv2.resize(misc.imread('TestImages/test2.jpg'), MODEL_IMG_SIZE)
+    img4 = cv2.resize(misc.imread('TestImages/test3.jpg'), MODEL_IMG_SIZE)
+    test_images = np.array([img1, img2, img3, img4])
+    test_labels = np.array([1, 1, 0, 0])
+    metrics = model.evaluate(test_images, test_labels)
+    for metric_i in range(len(model.metrics_names)):
+        print('Loss: {}'.format(metrics))
+
+
 def draw_boxes(img, windows, color=(0, 0, 255), thick=6):
     for window in windows:
         cv2.rectangle(img, window.bbox[0], window.bbox[1], color, thick)
@@ -96,14 +101,19 @@ def slide_window(img_shape, x_start_stop, y_start_stop, xy_window, xy_overlap, w
     if y_stop is None:
         y_stop = img_height
 
+    #print('Boundaries: ({0}, {1}); ({2}, {3})'.format(x_start, y_start, x_stop, y_stop))
+
     x_span = x_stop - x_start
     y_span = y_stop - y_start
+    #print('Span: {0}, {1}'.format(x_span, y_span))
 
     x_step = xy_window[0] * (1 - xy_overlap[0])
     y_step = xy_window[1] * (1 - xy_overlap[1])
+    #print('Step: {0}, {1}'.format(x_step, y_step))
 
     x_windows = np.int(x_span / x_step)
     y_windows = np.int(y_span / y_step)
+    #print('#Windows: {0}, {1}'.format(x_windows, y_windows))
 
     window_list = []
     for y in range(y_windows):
@@ -122,7 +132,16 @@ def create_search_windows(img_shape):
     near_windows = slide_window(img_shape, (None, None), (300, None), (256, 256), overlap, 1)
     mid_windows = slide_window(img_shape, (None, None), (400, 550), (128, 128), overlap, 2)
     far_windows = slide_window(img_shape, (None, None), (400, 464), (64, 64), overlap, 4)
+
     windows = near_windows + mid_windows + far_windows
+
+    #image = misc.imread('TestImages/test11.jpg')
+    #draw_boxes(image, near_windows, (0, 0, 255), 6)
+    #draw_boxes(image, mid_windows, (0, 255, 0), 6)
+    #draw_boxes(image, far_windows, (255, 0, 0), 6)
+    #misc.imsave('windows_no_overlap.jpg', image)
+    #exit(0)
+
     return windows
 
 
@@ -132,6 +151,7 @@ def get_window_images(img, windows):
         bbox = window.bbox
         window_img = img[bbox[0][1]:bbox[1][1], bbox[0][0]:bbox[1][0]]
         window_img = cv2.resize(window_img, MODEL_IMG_SIZE)
+        #misc.imsave('test.jpg', window_img)
         window_imgs.append(window_img)
     window_imgs = np.array(window_imgs)
     return window_imgs
@@ -146,9 +166,18 @@ def draw_labeled_bboxes(img, labels):
         cv2.rectangle(img, bbox[0], bbox[1], (0, 0, 255), 6)
 
 
-def find_vehicles(img):
-    global heat_map, iteration, last_labels
+iteration = 0
+heat_map = None
+last_labels = None
+max_hmap = 0
+HEAT_MAP_ITERATIONS = 7
+HEAT_MAP_THRESHOLD = 100
+MODEL_CAR_THRESHOLD = 0.75
 
+def find_vehicles(img):
+    global heat_map, iteration, last_labels, max_hmap
+
+    #img = misc.imread('TestImages/test6.jpg')
     if heat_map is None:
         heat_map = np.zeros_like(img[:, :, 0]).astype(np.float)
 
@@ -165,10 +194,21 @@ def find_vehicles(img):
 
     iteration += 1
     if iteration == HEAT_MAP_ITERATIONS:
+        max = np.max(heat_map)
+        if max > max_hmap:
+            max_hmap = max
+        #heat_map = np.clip(heat_map, 0, 255)
+        #misc.imsave('heatmap.jpg', heat_map)
         heat_map[heat_map < HEAT_MAP_THRESHOLD] = 0
+        #misc.imsave('heatmap_threshold.jpg', heat_map)
+
         labels = label(heat_map)
+        #misc.imsave('labels.jpg', labels[0])
+
         draw_labeled_bboxes(img, labels)
+        #misc.imsave('result.jpg', img)
         last_labels = labels
+
         iteration = 0
         heat_map = None
     elif last_labels is not None:
